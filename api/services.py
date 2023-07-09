0aab5ba5daf8e6505c83b07767d3e31b116aba66
voynow <voynow99@gmail.com>
2023-07-09 18:38:52 -0400

    MVP for dynamic portfolio rendering

---
+++ api/services.py
@@ -0,0 +1,30 @@
+import config
+from dotenv import load_dotenv
+from flask import url_for
+import os
+import requests
+load_dotenv()
+
+
+def fetch_portfolio():
+    """Fetch pinned projects from GitHub"""
+    endpoint = "https://api.github.com/graphql"
+    headers = {"Authorization": f"Bearer {os.environ['GH_TOKEN']}"}
+
+    response = requests.post(
+        endpoint,
+        json={"query": config.GH_GRAPHQL_QUERY % config.GITHUB_USERNAME},
+        headers=headers,
+    ).json()
+
+    edges = response["data"]["user"]["pinnedItems"]["edges"]
+    projects = [
+        {
+            "name": node["node"]["name"],
+            "description": node["node"]["description"],
+            "url": node["node"]["url"],
+            "imageUrl": url_for('static', filename=f'assets/{node["node"]["name"]}.png'),
+        }
+        for node in edges
+    ]
+    return projects
