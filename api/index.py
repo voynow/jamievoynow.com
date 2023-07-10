1813f1f941c3fb9ab04197b9b50b52ac856cb67b
voynow <voynow99@gmail.com>
2023-07-10 08:52:07 -0400

    Reverting index to test prod behavior

--- api/index.py
+++ api/index.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 import flask_cors
-import services
 
 app = Flask(__name__)
 flask_cors.CORS(app)
@@ -11,7 +10,20 @@ def healthchecker():
 
 @app.route("/api/portfolio", methods=["GET"])
 def portfolio():
-    return jsonify(services.fetch_portfolio())
+    # TODO: Replace this with actual data
+    data = [
+        {
+            "name": "Project 1",
+            "description": "This is a description for project 1.",
+            # "imageUrl": "https://example.com/image1.jpg"
+        },
+        {
+            "name": "Project 2",
+            "description": "This is a description for project 2.",
+            # "imageUrl": "https://example.com/image2.jpg"
+        }
+    ]
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
