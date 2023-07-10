08e3a65c7c2db2e44e14fa4d52c0765570688994
voynow <voynow99@gmail.com>
2023-07-10 09:32:16 -0400

    Plug github fetch fn back into API

--- api/index.py
+++ api/index.py
@@ -85,20 +85,7 @@ def healthchecker():
 
 @app.route("/api/portfolio", methods=["GET"])
 def portfolio():
-    # TODO: Replace this with actual data
-    data = [
-        {
-            "name": "Project 1",
-            "description": "This is a description for project 1.",
-            # "imageUrl": "https://example.com/image1.jpg"
-        },
-        {
-            "name": "Project 2",
-            "description": "This is a description for project 2.",
-            # "imageUrl": "https://example.com/image2.jpg"
-        }
-    ]
-    return jsonify(data)
+    return jsonify(fetch_portfolio())
 
 if __name__ == "__main__":
     app.run()
