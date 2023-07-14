81e83fb65d99ea60dcf9ba2c1c42d9565feaa13d
voynow <voynow99@gmail.com>
2023-07-14 17:49:06 -0400

    Finally feels like something I am proud of

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -6,13 +6,13 @@ export default function Navbar() {
     return (
         <nav className="bg-[rgba(var(--background-start-rgb), 0.9)] backdrop-blur-lg py-4 px-16 sticky top-0 z-50">
             <div className="flex items-center justify-between">
-                <div className="text-white text-2xl font-bold hover:text-blue-300">
-                    <Link href="/">
-                        <span role="img" aria-label="emoji">🚀</span> Jamie Voynow
-                    </Link>
-                </div>
-
-                <div className="flex space-x-8"> 
+                <Link href="/">
+                    <div className="text-white text-2xl font-bold hover:text-blue-300 flex items-center">
+                        <img src="/images/headshot.jpg" alt="Jamie Voynow" className=" mr-4 w-16 h-16 rounded-full object-cover"/>
+                        <span className="text-3xl">Jamie Voynow</span>
+                    </div>
+                </Link>
+                <div className="flex space-x-8">
                     <a href="https://twitter.com/JamieVoynow" target="_blank" rel="noopener noreferrer" className="nav-icon">
                         <FontAwesomeIcon icon={faTwitter} className="text-white hover:text-blue-300 " />
                     </a>
