49138ace419f0fcc0876e7ae8ff8b13b98ef6485
voynow <voynow99@gmail.com>
2023-07-17 08:21:36 -0400

    Recommended queries + error handling

--- app/ChatInterface.tsx
+++ app/ChatInterface.tsx
@@ -20,29 +20,72 @@ const ChatInterface = ({ project }: ChatInterfaceProps) => {
     const [message, setMessage] = useState<string>('');
     const [messages, setMessages] = useState<Message[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [queryIndex, setQueryIndex] = useState<number>(0);
     const inputRef = useRef<HTMLInputElement>(null);
 
+    const recommendedQueries = [
+        "Choose an important feature and explain it like I'm five.",
+        "What technologies were used in this project?",
+        "How can I contribute to this project? Give one simple idea.",
+        "What is the most interesting thing about this project?",
+        "Which part of the project is technically challenging?",
+    ];
+
+    const [error, setError] = useState<string | null>(null);
+
     const handleSend = async () => {
+        setError(null);
         if (message.trim()) {
             console.log('Sending message:', message);
             setIsLoading(true);
             setMessages(prevMessages => [...prevMessages, { text: message, sender: 'user' }]);
             setMessage('');
+            try {
+                const response = await fetch('/api/chat', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ message: message, project: project.name })
+                });
+                if (!response.ok) {
+                    throw new Error(`${response.status} ${response.statusText}`);
+                }
+                const responseData = await response.json();
+                console.log('Received response:', responseData);
+                setMessages(prevMessages => [...prevMessages, { text: responseData, sender: 'bot' }]);
+                setIsLoading(false);
+            } catch (error) {
+                console.error('Error sending message:', error);
+                setError('An error occurred while sending your message. Please try again.');
+                setIsLoading(false);
+            }
+        }
+    };
+
+    const handleRecommendedQueryClick = async (query: string) => {
+        setError(null);
+        console.log('Sending message:', query);
+        setIsLoading(true);
+        setMessages(prevMessages => [...prevMessages, { text: query, sender: 'user' }]);
+        try {
             const response = await fetch('/api/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ message: message, project: project.name })
+                body: JSON.stringify({ message: query, project: project.name })
             });
             if (!response.ok) {
-                console.error('Error sending message:', response.status, response.statusText);
-                setIsLoading(false);
-                return;
+                throw new Error(`${response.status} ${response.statusText}`);
             }
             const responseData = await response.json();
             console.log('Received response:', responseData);
             setMessages(prevMessages => [...prevMessages, { text: responseData, sender: 'bot' }]);
+            setIsLoading(false);
+        } catch (error) {
+            console.error('Error sending message:', error);
+            setError('An error occurred while sending your message. Please try again or try a different query.');
             setIsLoading(false);
         }
     };
@@ -50,6 +93,14 @@ const ChatInterface = ({ project }: ChatInterfaceProps) => {
     useEffect(() => {
         if (!isLoading) inputRef.current?.focus();
     }, [isLoading]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setQueryIndex((prevIndex) => (prevIndex + 1) % recommendedQueries.length);
+        }, 8000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div className="h-full flex flex-col bg-gradient-to-r from-secondary to-tertiary text-white shadow overflow-hidden sm:rounded-lg p-4">
@@ -72,15 +123,24 @@ const ChatInterface = ({ project }: ChatInterfaceProps) => {
                             </div>
                         </div>
                     ))}
-
+                    {error && (
+                        <div className="mt-2 text-sm text-red-500">
+                            {error}
+                        </div>
+                    )}
                     {isLoading && (
-                        <div className="self-start text-gray-500 flex items-center mb-2 ml-4 space-x-1">
+                        <div className="self-start text-gray-400 flex items-center mb-2 ml-4 space-x-1">
                             <div className="dot-typing"></div>
                         </div>
                     )}
                 </div>
             </div>
-            <div className="flex mt-4 border-t border-tertiary pt-4">
+            <div className="flex mt-4 border-t border-tertiary pt-2">
+                <div className="text-sm text-gray-400">
+                    Try asking: <button onClick={() => handleRecommendedQueryClick(recommendedQueries[queryIndex])} className="text-gray-200 underline hover:text-blue-300 focus:outline-none">{recommendedQueries[queryIndex]}</button>
+                </div>
+            </div>
+            <div className="flex border-tertiary pt-2">
                 <input
                     ref={inputRef}
                     type="text"
