b28a3fe0672915cf352055fa08bf9f05eb3808cf
voynow <voynow99@gmail.com>
2023-07-29 19:10:38 -0400

    Mobile friendly portfolio page updates

--- app/Portfolio.tsx
+++ app/Portfolio.tsx
@@ -29,9 +29,10 @@ const Portfolio = ({ className }: PortfolioProps) => {
                 console.error('Error fetching portfolio data:', err);
             });
     }, []);
+
     return (
         <div className={`${className} bg-secondary py-4 px-4 sm:px-6 lg:px-8`}>
-            <h1 className="text-3xl lg:text-6xl font-bold text-gray-800 text-center">
+            <h1 className="text-3xl lg:text-6xl font-bold text-gray-700 text-center">
                 Portfolio
             </h1>
             {loading ? (
@@ -39,23 +40,22 @@ const Portfolio = ({ className }: PortfolioProps) => {
                     <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary"></div>
                 </div>
             ) : (
-                <div className="mt-6 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <div className="mt-6 grid gap-6 grid-cols-2 lg:grid-cols-4">
                     {portfolio.map((project, index) => (
                         <Link href={`/project/${project.name}`} key={index} passHref>
                             <div className="rounded-xl border-2 border-gray-900 transition hover:scale-105 cursor-pointer hover:text-blue-200">
                                 {project.imageUrl && (
-                                    <div className="relative overflow-hidden border-2 border-gray-900 rounded-t-xl image-container h-64">
-                                        <img src={`/images/${project.imageUrl}`} alt={project.name} className="w-full h-full object-cover object-position:center portfolio-image hover:contrast-125" />
+                                    <div className={`h-36 sm:h-48 md:h-64 lg:h-72 relative overflow-hidden border-2 border-gray-900 rounded-t-xl image-container`}>
+                                        <img src={`/images/${project.imageUrl}`} alt={project.name} className="absolute top-0 left-0 w-full h-full object-cover object-position:center portfolio-image hover:contrast-125" />
                                     </div>
                                 )}
                                 <div className="mt-2 p-4">
-                                    <h2 className="text-xl font-semibold text-primary mb-2">{project.name}</h2>
-                                    <div className="description-container text-gray-500">{project.description}</div>
+                                    <h2 className="text-lg sm:text-xl md:text-2xl font-semibold text-primary mb-2">{project.name}</h2>
+                                    <div className="text-gray-400 text-sm sm:text-base md:text-lg line-clamp-2">{project.description}</div>
                                 </div>
                             </div>
                         </Link>
                     ))}
-
                 </div>
             )}
         </div>
