8a1ed17a4b5038bac6a72817b8f665df43ca2cfc
voynow <voynow99@gmail.com>
2023-07-29 20:32:07 -0400

    Testing out mobile chat update

--- app/ChatInterface.tsx
+++ app/ChatInterface.tsx
@@ -106,17 +106,17 @@ const ChatInterface = ({ project }: ChatInterfaceProps) => {
         <div className="h-full flex flex-col bg-gradient-to-r from-secondary to-tertiary text-white shadow overflow-hidden sm:rounded-lg p-4">
             <div className="flex-grow overflow-y-auto pb-4">
                 <div className="flex justify-between items-center mb-4">
-                    <h2 className="text-3xl font-bold text-primary">{project.name}</h2>
-                    <a href={project.url} target="_blank" rel="noopener noreferrer" className="bg-primary hover:bg-blue-300 rounded-lg text-white px-4 py-2 border-2 border-gray-300 flex items-center">
+                    <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-primary">{project.name}</h2>
+                    <a href={project.url} target="_blank" rel="noopener noreferrer" className="bg-primary hover:bg-blue-300 rounded-lg text-white px-3 py-2 border-2 border-gray-300 flex items-center">
                         <span>Github</span>
                         <FontAwesomeIcon icon={faExternalLinkAlt} className="ml-2" />
                     </a>
                 </div>
-                <p className='text-gray-400 mb-4'>{project.description}</p>
+                <p className='text-gray-400 text-xs sm:text-sm md:text-base mb-4'>{project.description}</p>
                 <div className="border-t border-tertiary mt-4 p-4">
                     {messages.map((msg, idx) => (
                         <div key={idx} className={`flex w-full ${msg.sender === 'user' ? 'justify-end' : 'justify-start'} mb-2`}>
-                            <div className={`p-2 rounded-lg border border-gray-800 ${msg.sender === 'user' ? 'bg-primary text-white ml-20' : 'bg-tertiary text-white mr-20'}`}>
+                            <div className={`p-2 rounded-lg border border-gray-800 text-xs sm:text-sm md:text-base ${msg.sender === 'user' ? 'bg-primary text-white ml-20' : 'bg-tertiary text-white mr-20'}`}>
                                 <ReactMarkdown className={`px-4 py-2 ${msg.sender === 'user' ? 'rounded-br-none' : 'rounded-bl-none'}`}>
                                     {msg.text}
                                 </ReactMarkdown >
@@ -136,7 +136,7 @@ const ChatInterface = ({ project }: ChatInterfaceProps) => {
                 </div>
             </div>
             <div className="flex mt-4 border-t border-tertiary pt-2">
-                <div className="text-sm text-gray-400">
+                <div className="text-xs sm:text-sm md:text-base text-gray-400">
                     Try asking: <button onClick={() => handleRecommendedQueryClick(recommendedQueries[queryIndex])} className="text-gray-200 underline hover:text-blue-300 focus:outline-none">{recommendedQueries[queryIndex]}</button>
                 </div>
             </div>
@@ -147,16 +147,16 @@ const ChatInterface = ({ project }: ChatInterfaceProps) => {
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
                     onKeyDown={(e) => { if (e.key === 'Enter') handleSend() }}
-                    className="text-black flex-grow rounded-l-lg px-4 py-2 border-t-0 border-r-0 border-b-0 outline-none bg-gray-300 placeholder-gray-400"
+                    className="text-s sm:text-m text-black flex-grow rounded-l-lg px-2 sm:px-4 py-1 sm:py-2 border-t-0 border-r-0 border-b-0 outline-none bg-gray-300 placeholder-gray-400"
                     placeholder="Enter your query here..."
                     disabled={isLoading}
                 />
-
-                <button onClick={handleSend} disabled={isLoading} className="bg-primary hover:bg-blue-300 rounded-r-lg text-white px-4 py-2 border-2 border-gray-300">
+                <button onClick={handleSend} disabled={isLoading} className="bg-primary hover:bg-blue-300 rounded-r-lg text-white px-3 py-2 border-2 border-gray-300">
                     Send
                 </button>
             </div>
         </div>
+
     );
 }
 
